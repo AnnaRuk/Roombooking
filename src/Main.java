268b8fa742c55f425ec0870db9f049ffb293278d
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-05-09 19:42:08 +0200

    add Class Room and Class RoomCategory

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Room r1 = new Room(1,2,RoomCategory.STANDARD);
+        System.out.println(r1);
     }
 }
---
+++ src/Room.java
@@ -0,0 +1,24 @@
+public class Room {
+
+    private int id; /// a room id
+    private int number;
+    private int capacity; //емкость
+    RoomCategory category;
+
+    public Room(int number, int capacity, RoomCategory category) {
+        this.id = number;
+        this.number = number;
+        this.capacity = capacity;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", number=" + number +
+                ", capacity=" + capacity +
+                ", category=" + category +
+                '}';
+    }
+}
